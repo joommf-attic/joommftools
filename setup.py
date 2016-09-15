838731f1e5c3b8c3bf1dc20dbd77b7a531611c6f
Ryan Pepper <ryan.pepper@soton.ac.uk>
2016-09-15 14:37:58 +0100

    Add setup.p

---
+++ setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+
+setup(name='joommftools',
+      version='0.1',
+      description='Visualisation tools for micromagnetism',
+      author='Computational Modelling Group',
+      author_email='fangohr@soton.ac.uk',
+      url='http://github.com/joommf/tools',
+      packages=['joommftools'],
+      install_requires=[
+          'finitedifferencefield',
+          'oommffield',
+          'holoviews'
+      ],
+      classifiers=[
+          'License :: OSI Approved :: BSD License',
+          'Programming Language :: Python :: 3',
+      ]
+)
+
